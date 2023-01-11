c40cc137f54132e503d8094ebcf9c9de33553861
mohit-0310 <mohit21167@iiitd.ac.in>
2023-01-11 23:49:31 +0530

    wednesdayCommit

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,11 +1,13 @@
 let firstCard = 10
-let secondCard = 4
+let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 function start(){
+    sumEl.textContent = "Sum : " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
